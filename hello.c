9c3eea82131bde9cb34134f37ad8a5629cbddba3
Caiuby Freitas <caiubyfreitas@users.noreply.github.com>
2025-04-17 12:16:36 +0100

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,24 @@
+//
+// How to compile
+//
+// gcc hello.c -o hello -I/usr/local/include -L/usr/local/lib -lraylib -lm
+//
+
+
+#include <stdio.h>
+#include "raylib.h"
+
+int main(void){
+    InitWindow(640, 480, "raylib");
+    while (!WindowShouldClose()){
+        BeginDrawing();
+        {
+            ClearBackground(RAYWHITE);
+            DrawCircle(100, 100, 100, RED);
+            DrawText("Hello World", 320, 240, 60, LIGHTGRAY);
+        }
+        EndDrawing();
+    }
+    CloseWindow();
+    return 0;
+}
